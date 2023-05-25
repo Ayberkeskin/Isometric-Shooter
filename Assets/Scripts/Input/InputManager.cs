9b6d63ebf46d72deff25e3bb6625b2bb3b784565
Ayberk Keskin <81094165+Ayberkeskin@users.noreply.github.com>
2023-05-26 01:36:48 +0300

    +Joystick,+MainSetup,+DOTween

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] InputData input;
+    [SerializeField] FloatingJoystick joystick;
+
+    void Update()
+    {
+        input.Horizontal = joystick.Horizontal;
+        input.Vertical = joystick.Vertical;
+
+    }
+}
